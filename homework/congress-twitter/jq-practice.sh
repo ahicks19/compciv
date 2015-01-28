aa77cd8fbd924304478e4f29c5bc78ff26f46277
Alex Nicole Hicks <ahicks19@corn31.stanford.edu>
2015-01-27 16:02:37 -0800

    whatever

--- homework/congress-twitter/jq-practice.sh
+++ homework/congress-twitter/jq-practice.sh
@@ -1,2 +1,13 @@
 echo '1.'
 cat data-hold/legislators-current.json | jq '.[0] .name .last'
+
+echo '2.'
+cat data-hold/legislators-current.json | jq '.[111] .id .bioguide'
+
+echo '3.'
+cat data-hold/legislators-current.json | jq '.[13] .terms[2] .start'
+
+echo '5.'
+cat data-hold/legislators-current.json | \
+  jq '.[] .bio .birthday' | sort -r | head -n 10 | tr -d '"'
+
