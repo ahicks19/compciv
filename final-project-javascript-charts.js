484792ae9d773cd1d09ef22f52a1a3d644000bda
ahicks19 <ahicks19@stanford.edu>
2015-03-18 16:11:11 -0700

    Update and rename final-project.js to final-project-javascript-charts.js

---
+++ final-project-javascript-charts.js
@@ -0,0 +1,537 @@
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Total Snowfall in Tahoe City, CA by Year'
+        },
+        subtitle: {
+            text: 'Source: NOAA'
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Total Snowfall (inches)'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: 'Total Snowfall: <b>{point.y:.1f} inches</b>'
+        },
+        series: [{
+            name: 'Population',
+            data: [
+                ['1964', 320.5],
+                ['1965', 233.6],
+                ['1966', 133.3],
+                ['1967', 361.4],
+                ['1968', 232.0],
+                ['1969', 234.0],
+                ['1970', 219.5],
+                ['1971', 175.4],
+                ['1972', 138.9],
+                ['1973', 183.5],
+                ['1974', 136.8],
+                ['1975', 221.5],
+                ['1976', 63.0],
+                ['1977', 105.3],
+                ['1978', 175.6],
+                ['1979', 183.4],
+                ['1980', 143.0],
+                ['1981', 115.9],
+                ['1982', 280.7],
+                ['1983', 232.8],
+                ['1984', 141.8],
+                ['1985', 188.9],
+                ['1986', 114.0],
+                ['1987', 93.8],
+                ['1988', 135.5],
+                ['1989', 133.8],
+                ['1990', 120.9],
+                ['1991', 87.5],
+                ['1992', 137.5],
+                ['1993', 135.0],
+                ['1994', 112.0],
+                ['1995', 246.0],
+                ['1996', 321.5],
+                ['1997', 120.5],
+                ['1998', 260.9],
+                ['1999', 232.0],
+                ['2000', 155.5],
+                ['2001', 202.4],
+                ['2002', 193.1],
+                ['2003', 172.0],
+                ['2004', 208.7],
+                ['2005', 187.1],
+                ['2006', 208.4],
+                ['2007', 124.6],
+                ['2008', 212.5],
+                ['2009', 158.0],
+                ['2010', 228.7],
+                ['2011', 218.0],
+                ['2012', 185.1],
+                ['2013', 41.5],
+                ['2014', 46.0],       
+                
+                
+            ],
+            dataLabels: {
+                enabled: false,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        }]
+    });
+    
+    
+    
+    
+    $(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Total Snowfall in Steamboat Springs, CO by Year'
+        },
+        subtitle: {
+            text: 'Source: NOAA'
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Total Snowfall (inches)'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: 'Total Snowfall: <b>{point.y:.1f} inches</b>'
+        },
+        series: [{
+            name: 'Population',
+            data: [
+                ['1964', 217.7],
+                ['1965', 174.7],
+                ['1966', 120.0],
+                ['1967', 132.0],
+                ['1968', 177.3],
+                ['1969', 153.8],
+                ['1970', 165.6],
+                ['1971', 197.9],
+                ['1972', 132.5],
+                ['1973', 198.8],
+                ['1974', 224.0],
+                ['1975', 208.1],
+                ['1976', 123.6],
+                ['1977', 154.4],
+                ['1978', 205.7],
+                ['1979', 189.7],
+                ['1980', 161.5],
+                ['1981', 160.1],
+                ['1982', 176.5],
+                ['1983', 251.0],
+                ['1984', 160.6],
+                ['1985', 187.5],
+                ['1986', 54.5],
+                ['1987', 91.6],
+                ['1988', 197.2],
+                ['1989', 159.4],
+                ['1990', 139.0],
+                ['1991', 165.3],
+                ['1992', 97.8],
+                ['1993', 187.7],
+                ['1994', 129.5],
+                ['1995', 147.0],
+                ['1996', 301.9],
+                ['1997', 153.4],
+                ['1998', 136.2],
+                ['1999', 125.9],
+                ['2000', 145.0],
+                ['2001', 91.2],
+                ['2002', 108.4],
+                ['2003', 147.7],
+                ['2004', 81.7],
+                ['2005', 194.7],
+                ['2006', 143.0],
+                ['2007', 156.6],
+                ['2008', 243.9],
+                ['2009', 205.2],
+                ['2010', 209.1],
+                ['2011', 189.4],
+                ['2012', 172.1],
+                ['2013', 208.9],
+                ['2014', 182.7],       
+                
+                
+            ],
+            dataLabels: {
+                enabled: false,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        }]
+    });
+});
+
+
+
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Total Snowfall in Iron Mountain, MI by Year'
+        },
+        subtitle: {
+            text: 'Source: NOAA'
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Total Snowfall (inches)'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: 'Total Snowfall: <b>{point.y:.1f} inches</b>'
+        },
+        series: [{
+            name: 'Population',
+            data: [
+                ['1964', 42.4],
+                ['1965', 76.2],
+                ['1966', 55.6],
+                ['1967', 77.9],
+                ['1968', 55.9],
+                ['1969', 60.9],
+                ['1970', 60.6],
+                ['1971', 99.0],
+                ['1972', 111.4],
+                ['1973', 28.0],
+                ['1974', 36.8],
+                ['1975', 74.0],
+                ['1976', 89.8],
+                ['1977', 89.6],
+                ['1978', 51.5],
+                ['1979', 80.4],
+                ['1980', 48.0],
+                ['1981', 42.1],
+                ['1982', 47.5],
+                ['1983', 62.1],
+                ['1984', 43.3],
+                ['1985', 90.5],
+                ['1986', 44.2],
+                ['1987', 41.9],
+                ['1988', 47.4],
+                ['1989', 66.9],
+                ['1990', 45.0],
+                ['1991', 48.5],
+                ['1992', 83.3],
+                ['1993', 51.6],
+                ['1994', 47.3],
+                ['1995', 66.0],
+                ['1996', 105.7],
+                ['1997', 60.6],
+                ['1998', 71.1],
+                ['1999', 48.4],
+                ['2000', 80.1],
+                ['2001', 41.3],
+                ['2002', 60.2],
+                ['2003', 53.6],
+                ['2004', 86.6],
+                ['2005', 54.7],
+                ['2006', 47.2],
+                ['2007', 46.0],
+                ['2008', 83.2],
+                ['2009', 64.4],
+                ['2010', 26.7],
+                ['2011', 37.7],
+                ['2012', 51.3],
+                ['2013', 65.5],
+                ['2014', 99.2],       
+                
+                
+            ],
+            dataLabels: {
+                enabled: false,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        }]
+    });
+});
+
+
+
+
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Total Snowfall in Grandfather Mountain, NC by Year'
+        },
+        subtitle: {
+            text: 'Source: NOAA'
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Total Snowfall (inches)'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: 'Total Snowfall: <b>{point.y:.1f} inches</b>'
+        },
+        series: [{
+            name: 'Population',
+            data: [
+                ['1964', 43.9],
+                ['1965', 40.7],
+                ['1966', 74.0],
+                ['1967', 28.5],
+                ['1968', 58.7],
+                ['1969', 79.0],
+                ['1970', 49.5],
+                ['1971', 61.4],
+                ['1972', 44.0],
+                ['1973', 38.1],
+                ['1974', 45.0],
+                ['1975', 39.9],
+                ['1976', 33.1],
+                ['1977', 86.0],
+                ['1978', 64.4],
+                ['1979', 68.9],
+                ['1980', 53.9],
+                ['1981', 98.9],
+                ['1982', 39.1],
+                ['1983', 89.5],
+                ['1984', 46.3],
+                ['1985', 55.9],
+                ['1986', 36.4],
+                ['1987', 94.6],
+                ['1988', 47.5],
+                ['1989', 61.4],
+                ['1990', 19.0],
+                ['1991', 34.0],
+                ['1992', 58.5],
+                ['1993', 77.7],
+                ['1994', 19.5],
+                ['1995', 48.4],
+                ['1996', 118.5],
+                ['1997', 47.8],
+                ['1998', 53.2],
+                ['1999', 64.5],
+                ['2000', 62.0],
+                ['2001', 41.5],
+                ['2002', 32.1],
+                ['2003', 60.9],
+                ['2004', 38.5],
+                ['2005', 41.9],
+                ['2006', 31.5],
+                ['2007', 39.2],
+                ['2008', 28.9],
+                ['2009', 26.6],
+                ['2010', 25.6],
+                ['2011', 35.2],
+                ['2012', 7.2],
+                ['2013', 4.0],
+                ['2014', 12.5],       
+                
+                
+            ],
+            dataLabels: {
+                enabled: false,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        }]
+    });
+});
+
+
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Total Snowfall in Mohonk Lake, NY by Year'
+        },
+        subtitle: {
+            text: 'Source: NOAA'
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Total Snowfall (inches)'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: 'Total Snowfall: <b>{point.y:.1f} inches</b>'
+        },
+        series: [{
+            name: 'Population',
+            data: [
+                ['1964', 65.7],
+                ['1965', 56.8],
+                ['1966', 86.2],
+                ['1967', 114.8],
+                ['1968', 46.1],
+                ['1969', 88.1],
+                ['1970', 69.9],
+                ['1971', 92.4],
+                ['1972', 80.5],
+                ['1973', 36.8],
+                ['1974', 54.2],
+                ['1975', 61.0],
+                ['1976', 55.4],
+                ['1977', 101.8],
+                ['1978', 91.2],
+                ['1979', 42.4],
+                ['1980', 52.5],
+                ['1981', 48.3],
+                ['1982', 70.1],
+                ['1983', 82.0],
+                ['1984', 75.9],
+                ['1985', 52.4],
+                ['1986', 71.6],
+                ['1987', 67.0],
+                ['1988', 57.6],
+                ['1989', 32.2],
+                ['1990', 52.3],
+                ['1991', 36.3],
+                ['1992', 48.5],
+                ['1993', 71.7],
+                ['1994', 78.3],
+                ['1995', 70.7],
+                ['1996', 108.1],
+                ['1997', 70.6],
+                ['1998', 26.4],
+                ['1999', 35.8],
+                ['2000', 72.3],
+                ['2001', 75.2],
+                ['2002', 62.7],
+                ['2003', 82.9],
+                ['2004', 50.2],
+                ['2005', 91.1],
+                ['2006', 32.6],
+                ['2007', 63.3],
+                ['2008', 57.6],
+                ['2009', 47.6],
+                ['2010', 79.7],
+                ['2011', 74.2],
+                ['2012', 37.1],
+                ['2013', 57.3],
+                ['2014', 64.5],       
+                
+                
+            ],
+            dataLabels: {
+                enabled: false,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        }]
+    });
+});
